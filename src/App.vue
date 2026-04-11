ad638360609857bd332b78cca68a8dff8b6cef39
Sean Li <9913851+saifahn@users.noreply.github.com>
2026-04-11 16:48:41 +0900

    feat: add base functionality

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const monthlyRent = ref(0)
+const reikin = ref(0)
+const lengthOfStayInMonths = ref(0)
+
+const normalizedRent = computed(() => {
+  if (lengthOfStayInMonths.value === 0) return 0
+  return (
+    (monthlyRent.value * lengthOfStayInMonths.value + reikin.value) / lengthOfStayInMonths.value
+  )
+})
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <label>
+    <span>Monthly Rent:</span>
+    <input v-model.number="monthlyRent" type="number" />
+  </label>
+  <label>
+    <span>Reikin:</span>
+    <input v-model.number="reikin" type="number" />
+  </label>
+  <label>
+    <span>Length of Stay (months):</span>
+    <input v-model.number="lengthOfStayInMonths" type="number" />
+  </label>
+
+  <p>Normalized Rent: {{ normalizedRent }}</p>
 </template>
 
 <style scoped></style>
